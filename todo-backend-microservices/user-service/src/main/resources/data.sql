99c18be0a84b0cd551e2f3bf4bda2024b83be6ec
Gustavo Lira <gu.lirar23@gmail.com>
2023-04-18 15:10:32 -0500

    config: add configuration to microservice yml file and initial data
    - add eureka configuration
    - spring configuration for connecting to database
    - set up server port

---
+++ todo-backend-microservices/user-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (USER_NAME ,EMAIL, USER_PASSWORD, USER_ROLE)  VALUES ('Gustavo Lira','gustavo@email.com','1234gus','ADMIN'); /*  VALUES ('gustavo@email.com','Gustavo','Lira','$2a$12$lLDG6ir1eRwdxLwR47YxPOdPHSsmQFIQdWujNnsmHd3/4l48JcE5e','ADMIN'); 1234gus */
+INSERT INTO users (USER_NAME ,EMAIL, USER_PASSWORD, USER_ROLE) VALUES ('Adan Alvarez','adan@email.com','1234adan','ADMIN'); /* 1234adan */
+INSERT INTO users (USER_NAME ,EMAIL, USER_PASSWORD, USER_ROLE) VALUES ('Andres Avalos','andres@email.com','1234andres','ADMIN'); /* 1234andres */
+INSERT INTO users (USER_NAME ,EMAIL, USER_PASSWORD, USER_ROLE) VALUES ('Bryan Gongora','bryan@email.com','1234bryan','REGULAR'); /*  1234bryan */
+INSERT INTO users (USER_NAME ,EMAIL, USER_PASSWORD, USER_ROLE) VALUES ('Ricardo Zapata','ricardo@email.com','1234ricardo','REGULAR'); /* 1234ricardo */
