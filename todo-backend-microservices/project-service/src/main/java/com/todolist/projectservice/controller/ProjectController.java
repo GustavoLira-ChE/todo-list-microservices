ddff8edf2245ddd6708dc6a9122c8500db125d63
Gustavo Lira <gu.lirar23@gmail.com>
2023-04-19 17:46:13 -0500

    fix: controller to save new register lacks RequestBody annotation

--- todo-backend-microservices/project-service/src/main/java/com/todolist/projectservice/controller/ProjectController.java
+++ todo-backend-microservices/project-service/src/main/java/com/todolist/projectservice/controller/ProjectController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -49,7 +50,7 @@ public class ProjectController {
     }
 
     @PostMapping
-    public ResponseEntity<ProjectEntity> save(ProjectEntity project) {
+    public ResponseEntity<ProjectEntity> save(@RequestBody ProjectEntity project) {
         try{
             ProjectEntity newProject = this.projectService.save(project);
             return ResponseEntity.ok(newProject);
