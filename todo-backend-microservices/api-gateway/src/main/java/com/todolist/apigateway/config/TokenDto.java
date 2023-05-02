3d2cd83be282a7ceaf349cd8c4acfd34b6ac0790
Gustavo Lira <gu.lirar23@gmail.com>
2023-05-02 13:11:08 -0500

    add: configuration to pass all the request throught auth-service

---
+++ todo-backend-microservices/api-gateway/src/main/java/com/todolist/apigateway/config/TokenDto.java
@@ -0,0 +1,13 @@
+package com.todolist.apigateway.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenDto {
+
+    private String token;
+}
