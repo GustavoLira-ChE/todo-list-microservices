536da3821feecf06381d36cf1f51fed44c795077
Gustavo Lira <gu.lirar23@gmail.com>
2023-05-02 14:38:07 -0500

    fix: uri to connect with auth-service

--- todo-backend-microservices/api-gateway/src/main/java/com/todolist/apigateway/config/AuthFilter.java
+++ todo-backend-microservices/api-gateway/src/main/java/com/todolist/apigateway/config/AuthFilter.java
@@ -30,7 +30,7 @@ public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> 
                 return onError(exchange, HttpStatus.BAD_REQUEST);
             return webClient.build()
                     .post()
-                    .uri("http://auth-service/api/auth/validate?token=" + chunks[1])
+                    .uri("http://auth-service/validate?token=" + chunks[1])
                     .retrieve().bodyToMono(TokenDto.class)
                     .map(t -> {
                         t.getToken();
