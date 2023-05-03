a7d46a092f05a13f50c50f47d0834bd45f9f4bb4
Gustavo Lira <gu.lirar23@gmail.com>
2023-05-03 13:09:33 -0500

    add: module rules for babel fix typo error

--- todo-frontend/webpack.config.js
+++ todo-frontend/webpack.config.js
@@ -1,17 +1,28 @@
 const path = require('path');
 
-module.export = {
-    entry: './src/idex.js',
+module.exports = {
+    entry: './src/index.js',
     output: {
         path: path.resolve(__dirname,"dist"),
         filename: "bundle.js",
     },
     resolve: {
-        extensions: ['.js','.jsx'], 
+        extensions: ['.js','.jsx'],
     },
     mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                },
+            },
+        ]
+    },
     devServer: {
-        static: path.join(__dirname,"dist"),
+        static: path.join(__dirname,'dist'),
         port: 3001,
         historyApiFallback: true,
     }
