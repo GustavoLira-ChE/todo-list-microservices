201b9c552f5ce614b21c255364e70e65a7912312
Gustavo Lira <gu.lirar23@gmail.com>
2023-05-05 12:23:33 -0500

    add: final configuration for styling

--- todo-frontend/src/index.js
+++ todo-frontend/src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
+import "bootstrap";
+import "./styles/_custom.scss";
 import App from './App';
 
 const root = createRoot( document.getElementById('app')); 
