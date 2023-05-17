0ce774aaa2758e96ade0c2c5649dc32a8ffb8947
Gustavo Lira <gu.lirar23@gmail.com>
2023-05-17 18:57:39 -0500

    refactor: change store logic to userHook

--- todo-frontend/src/components/Login.jsx
+++ todo-frontend/src/components/Login.jsx
@@ -1,40 +1,34 @@
 import React, { useRef, useContext } from 'react';
+import { useSelector } from 'react-redux';
 import "./../styles/Login.scss";
 import { Link, useNavigate } from 'react-router-dom';
-import { useDispatch } from "react-redux";
-import { startSession } from '../state/reducer/userSession.reducer';
 import UserTokenContext from '../context/UserTokenContext';
-
-
 
 const Login = () => {
 
-    const {token, getToken} = useContext(UserTokenContext);
+    const { getToken } = useContext(UserTokenContext);
     const loginForm = useRef(null);
-    const dispatch = useDispatch();
     let navigate = useNavigate();
+    const state = useSelector(state => state)
 
-    const handleLoginFormValues = (event) => {
-        event.preventDefault();
+    const handleLoginFormValues = () => {
         const formData = new FormData(loginForm.current);
         const data = {
             email: formData.get("email"),
             password: formData.get("password")
         }
-        getToken(data);
-        const userSessionInfo = {
-            email: data.email,
-            token: token
+        let validate = getToken(data);
+        console.log(state, validate);
+        if(validate){
+            navigate("/");
         }
-        dispatch(startSession(userSessionInfo));
-        navigate("/");
     }
 
     return (
         <div className="login">
             <div className="login-container mb-3">
                 <h1>Login to your account</h1>
-                <form ref={loginForm}>
+                <form ref={loginForm} onSubmit={(e) => e.preventDefault()}>
                     <div className="login-field input-group">
                         <label className='form-label' htmlFor="">Email: </label><br />
                         <input type="email" className='form-control'
@@ -44,7 +38,7 @@ const Login = () => {
                         <label className='form-label' htmlFor="">Password: </label><br />
                         <input type="password" className='form-control' name='password'/>
                     </div>
-                    <input className="login-submit-button" type="submit" value="Login" onClick={(event)=> handleLoginFormValues(event)} />
+                    <button className="login-submit-button" onClick={()=> handleLoginFormValues()}>Login</button>
                 </form>
                 <p>Need an account? <Link to="/signup">SIGN UP</Link></p>
             </div>
