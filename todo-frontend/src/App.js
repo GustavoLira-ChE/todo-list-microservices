88b512ad9fc420464ec3ffb6e820f8600e49b24c
Gustavo Lira <gu.lirar23@gmail.com>
2023-05-17 16:50:43 -0500

    add: user provider and store provider

--- todo-frontend/src/App.js
+++ todo-frontend/src/App.js
@@ -3,14 +3,22 @@ import { BrowserRouter } from "react-router-dom";
 import Header from './components/Header';
 import MainRoutes from './routes/MainRoutes';
 import Footer from './components/Footer';
+import store from "./state/store/userSession.store";
+import { Provider } from "react-redux";
+import { UserTokenProvider } from './context/UserTokenContext';
+
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <Header />
-            <MainRoutes />
-            <Footer />
-        </BrowserRouter>
+        <Provider store={store}>
+            <UserTokenProvider>
+                <BrowserRouter>
+                    <Header />
+                    <MainRoutes />
+                    <Footer />
+                </BrowserRouter>
+            </UserTokenProvider>
+        </Provider>
     );
 }
 
