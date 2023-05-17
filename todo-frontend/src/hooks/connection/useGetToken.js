cc34598bb6df5338023620eaa4b493d02b45d3ca
Gustavo Lira <gu.lirar23@gmail.com>
2023-05-17 18:58:18 -0500

    add: logic store for saving global state

--- todo-frontend/src/hooks/connection/useGetToken.js
+++ todo-frontend/src/hooks/connection/useGetToken.js
@@ -1,11 +1,14 @@
-import { useState } from "react";
 import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { startSession } from "../../state/reducer/userSession.reducer";
+
 
 const BACKEND_LOGIN_API = "http://localhost:8082/api/auth/login";
 
 const useGetToken = () => {
 
-    const [token, setToken] = useState(null);
+    const dispatch = useDispatch();
+    const state = useSelector(state => state);
 
     const getToken = (credentials) => {
         async function fecthToken() {
@@ -13,13 +16,22 @@ const useGetToken = () => {
                 user_email: credentials.email,
                 user_password: credentials.password
             })
-            setToken(response.data.token);
+            const userSessionInfo = {
+                email: credentials.email,
+                token: response.data.token
+            }
+            if(userSessionInfo.token != null){
+                dispatch(startSession(userSessionInfo));
+                console.log(dispatch(startSession(userSessionInfo)))
+                console.log(state);
+                return true;
+            }
+            return false;
         }
         fecthToken();
     }
 
     return {
-        token,
         getToken
     };
 };
